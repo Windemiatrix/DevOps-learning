df42a3acad349be95358e5a509531dd39902b795
Roman Martsev <r.martsev@gmail.com>
2020-11-10 00:02:56 +0300

    Changing file structure

--- otus-homeworks/homeworks/play-travis/run.sh
+++ otus-homeworks/homeworks/play-travis/run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 GROUP=NOGROUP
 BRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}
-#HOMEWORK_RUN=./otus-homeworks/homeworks/$BRANCH/run.sh
-HOMEWORK_RUN=./test.py
+HOMEWORK_RUN=./otus-homeworks/homeworks/$BRANCH/run.sh
 #REPO=https://github.com/express42/otus-homeworks.git
 REPO=https://github.com/Windemiatrix/DevOps-learning.git
 DOCKER_IMAGE=express42/otus-homeworks:0.7
