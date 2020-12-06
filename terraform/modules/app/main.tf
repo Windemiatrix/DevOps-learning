a98c7447f9566034db80d73a63a9ac9cc71a0a85
Roman Martsev <r.martsev@gmail.com>
2020-12-06 17:26:17 +0300

    Small changes in terraform scripts

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -25,11 +25,11 @@ resource "google_compute_instance" "app" {
     private_key = file(var.private_key_path)
   }
   provisioner "file" {
-    source      = "files/puma.service"
+    source      = "../files/puma.service"
     destination = "/tmp/puma.service"
   }
   provisioner "remote-exec" {
-    script = "files/deploy.sh"
+    script = "../files/deploy.sh"
   }
 }
 
--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -1,6 +1,6 @@
 provider "google" {
   # Версия провайдера
-  version = "~> 2.15"
+  #version = "~> 2.15"
   #ID проекта
   project = var.project
   region = var.region
